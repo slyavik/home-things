36e5997ab7b1a7a32d0ab0c4350b71d15e9a48ff
Slyavik <slyavikgl@gmail.com>
2019-08-11 16:16:16 +0200

    first

---
+++ edit.php
@@ -0,0 +1,34 @@
+<Html>
+<head>
+    <meta charset="UTF-8">
+    <title>Домшні речі</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="page"><hr class="hr1">
+    <div id="header"> Універсальний проект "Мої домашні речі" </div>
+    <div>
+        <ul class="menu">
+            <li><a href="index.php">Головна</a> </li>
+            <li><a href="edit.php">Редагування</a> </li>
+            <li><a href="#">Планування</a> </li>
+            <li><a href="#">Викинуті товари</a> </li>
+            <li><a href="log.php">Адмін</a> </li>
+        </ul>
+    </div>
+    <div id="clear"><hr class="hr2"></div>
+</div>
+<div id="content">
+        <div id="container">
+        <hr class= "hr01">
+        <table width="100%"><tr><td>
+                    <img src="images/noimages.png" width="75" align="left" hspace="20"  border="4"/>
+                    <h2 align="center"> Назва // Ціна // Номер </h2>
+                </td></tr></table>
+
+        <hr class="hr02">
+    </div></div>
+<div id="footer"><h2 align="center">Наші речі</h2></div>
+</body>
+</Html>
+
