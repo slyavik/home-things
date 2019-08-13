0fe7242ab5940ffc056668d11073a99761f17ffe
Slyavik <slyavikgl@gmail.com>
2019-08-13 14:39:46 +0200

    Connection Bootstrap end jQuery

--- edit.php
+++ edit.php
@@ -1,34 +1,14 @@
+<! Doctype html>
 <Html>
 <head>
     <meta charset="UTF-8">
-    <title>Домшні речі</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Homemade Things</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <script src="style/jquery.min.js"></script>
 </head>
 <body>
-<div id="page"><hr class="hr1">
-    <div id="header"> Універсальний проект "Мої домашні речі" </div>
-    <div>
-        <ul class="menu">
-            <li><a href="index.php">Головна</a> </li>
-            <li><a href="edit.php">Редагування</a> </li>
-            <li><a href="#">Планування</a> </li>
-            <li><a href="#">Викинуті товари</a> </li>
-            <li><a href="log.php">Адмін</a> </li>
-        </ul>
-    </div>
-    <div id="clear"><hr class="hr2"></div>
-</div>
-<div id="content">
-        <div id="container">
-        <hr class= "hr01">
-        <table width="100%"><tr><td>
-                    <img src="images/noimages.png" width="75" align="left" hspace="20"  border="4"/>
-                    <h2 align="center"> Назва // Ціна // Номер </h2>
-                </td></tr></table>
+<p>Edit</p>
 
-        <hr class="hr02">
-    </div></div>
-<div id="footer"><h2 align="center">Наші речі</h2></div>
 </body>
 </Html>
-
--- index.php
+++ index.php
@@ -1,10 +1,23 @@
+<! Doctype html>
 <Html>
 <head>
     <meta charset="UTF-8">
-    <title>Домшні речі</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Homemade Things</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <script src="style/jquery.min.js"></script>
 </head>
 <body>
+<script>
+    $(document).ready(function () {
+        $("#logo")
+            .animate({top: '30px', left:'30%', opacity: '0.5', fontSize:'2em'},2000)
+            .animate({opacity: '1'});
+    })
+</script>
+<div style="position: absolute" id="logo">
+    Home Things - Домашні речі</div>
+
 
 </body>
 </Html>
