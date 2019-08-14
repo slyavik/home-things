33216f42d5b0101aed78929da7cf92ceea395b47
Slyavik <slyavikgl@gmail.com>
2019-08-14 18:41:36 +0200

    menu

--- edit.php
+++ edit.php
@@ -1,14 +1,1 @@
-<! Doctype html>
-<Html>
-<head>
-    <meta charset="UTF-8">
-    <title>Homemade Things</title>
-    <link rel="stylesheet" type="text/css" href="style/style.css">
-    <link rel="stylesheet" href="style/bootstrap.min.css">
-    <script src="style/jquery.min.js"></script>
-</head>
-<body>
-<p>Edit</p>
-
-</body>
-</Html>
+edit
--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Homemade Things</title>
-    <link rel="stylesheet" type="text/css" href="style/style.css">
-    <link rel="stylesheet" href="style/bootstrap.min.css">
     <script src="style/jquery.min.js"></script>
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <script src="style/popper.min.js"></script>
+    <script src="style/bootstrap.min.js"></script>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <script>
@@ -18,6 +20,37 @@
 <div style="position: absolute" id="logo">
     Home Things - Домашні речі</div>
 
+<!-- navbar panel-------------------------- -->
+<nav class="navbar navbar-fixed-top navbar-expand-sm bg-dark navbar-dark navbar-my">
+    <a class="navbar-brand" href="#">
+        <img src="img/logo11.PNG" alt="logo" style="height: 40px"> Head
+    </a>
 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navbar links     -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="edit.php" class="nav-link">Edit</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">planing</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">thrown things</a>
+            </li>
+            <li class="nav-item">
+                <a href="log.php" class="nav-link">login</a>
+            </li>
+        </ul>
+        </div>
+</nav>
+
+<header class="header">
+<br><br><br><br><br><br><br><br><br><br>
+    jjjdjdjdjdjdjdjd
+</header>
 </body>
 </Html>
