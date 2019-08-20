8d33f7b55230dcc71f2d172246185990a311c701
Slyavik <slyavikgl@gmail.com>
2019-08-20 11:33:43 +0200

    main container

--- index.php
+++ index.php
@@ -17,11 +17,12 @@
             .animate({opacity: '1'});
     })
 </script>
-<div style="position: absolute" id="logo">
+
+<div id="logo">
     Home Things - Домашні речі</div>
 
 <!-- navbar panel-------------------------- -->
-<nav class="navbar navbar-fixed-top navbar-expand-sm bg-dark navbar-dark navbar-my">
+<nav class="navbar navbar-fixed-top navbar-expand-sm bg-dark navbar-dark sticky-top navbar-my">
     <a class="navbar-brand" href="#">
         <img src="img/logo11.PNG" alt="logo" style="height: 40px"> Head
     </a>
@@ -47,9 +48,83 @@
         </ul>
         </div>
 </nav>
+<div class="container container-my">
+<div class="row"> <!--main container-->
+    <div class="col-lg-8 col-xl-9 container-my">
+        <div class="row"> <!-- things container-->
+            <div class="col-xl-6 container-my2">
+                <div class="card mb-3 overlay" style="max-width: 540px;"> <!--Card-->
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="..." class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title 111</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-6 container-my2">
+                <div class="card mb-3 overlay" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="..." class="card-img" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title222</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+        <p>left</p>
+    </div>
+    <div class="order-first order-lg-0 col-lg-4 col-xl-3 container-my">
+        <div class="container">
+            <div class="row">
+
+                <div class="col align-self-center">
+                    One of three columns Right
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+    <p> Contener </p>
+</div>
+
 
 <header class="header">
 <br><br><br><br><br><br><br><br><br><br>
+    jjjdjdjdjdjdjdjd
+</header>
+<header class="header">
+    <br><br><br><br><br><br><br><br><br><br>
+    jjjdjdjdjdjdjdjd
+</header>
+<header class="header">
+    <br><br><br><br><br><br><br><br><br><br>
+    jjjdjdjdjdjdjdjd
+</header>
+<header class="header">
+    <br><br><br><br><br><br><br><br><br><br>
+    jjjdjdjdjdjdjdjd
+</header>
+<header class="header">
+    <br><br><br><br><br><br><br><br><br><br>
     jjjdjdjdjdjdjdjd
 </header>
 </body>
